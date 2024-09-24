c467c36de39bcb714d1d181764e09f44f3ffb367
Gustavo Henrique <143838010+Gushenp@users.noreply.github.com>
2024-09-24 14:41:48 +0000

    new file:   CineVerde/Pages/index.html new file:   CineVerde/Pages/modal.html new file:   CineVerde/Scripts/modal.js new file:   CineVerde/Scripts/script.js new file:   CineVerde/Styles/modal.css new file:   CineVerde/Styles/style.css new file:   CineVerde/files/background.png

---
+++ CineVerde/Scripts/modal.js
@@ -0,0 +1,60 @@
+const background  = document.getElementById('modal-background')
+const modalContainer = document.getElementById('modal-container')
+
+let currentMovie = [];
+
+background.addEventListener('click', function backgroundClickHandler(){
+    overlay.classList.remove('open');
+});
+
+function addCurrentMovieToList(){
+    const titles = movieList.map(movie => movie.imdbID)
+    try {
+        if (titles.includes(currentMovie.imdbID)){
+            throw new Error(`Este filme já se encontra na lista.`);
+        } else {
+        addToList(currentMovie)
+        updateUI(currentMovie)
+        updateLocalStorage()
+        closeModal()
+        };
+    } catch(error) {
+       console.log(error)
+       notie.alert({ type: 'error', text: error.message })
+    }
+};
+
+function creatmodal(data) {
+    currentMovie = data;
+    modalContainer.innerHTML = 
+    `
+        <h2 id="movie-title">${data.Title} - ${data.Year}</h2>
+        <section id="modal-body">
+            <img id="movie-poster" src="${data.Poster}" 
+            alt="poster do filme.">
+            <div id="movie-info">
+                <div id="movie-plot">
+                    <h4>Participantes</h4>
+                    <p>${data.Actors}</p>
+                </div>
+                <div id="movie-cast">
+                    <h4>${data.Plot} </h4>
+                    <p></p>
+                </div>
+                <div id="movie-genre">
+                    <h4>
+                        Genero
+                    </h4>
+                    <p>${data.Genre}</p>
+                </div>
+            </div>
+        </section>
+        <section id="modal-footer">
+                <button id="add-to-list" onclick='{addCurrentMovieToList()}'>Adicionar a lista</button>
+        </section> 
+    `
+}
+
+function closeModal(){
+    overlay.classList.remove("open");
+}
---
+++ CineVerde/Scripts/script.js
@@ -0,0 +1,85 @@
+const $searchButton = document.getElementById('search-button');
+const overlay = document.getElementById('modal-overlay');
+const movieName = document.getElementById('movie-name');
+const movieYear = document.getElementById('movie-year');
+const movieListContainer = document.getElementById('movie-list')
+
+
+//let movieList = [];
+let movieList = JSON.parse(localStorage.getItem("movieList")) ?? [];
+
+async function searchButtonClickHandler() {
+    try {
+        let url = `https://www.omdbapi.com/?apikey=27618e79&t=${movieNameParameterGenerator()}&y=${movieYearParameterGenerator()}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log('data:', data)
+        if (data.Error) {
+            throw new Error('Filme não encontrado.')
+        }
+        creatmodal(data);
+        overlay.classList.add('open'); } catch(error) {
+            console.log(error)
+            notie.alert({ type: 'error', text: error.message })
+        }
+}
+
+function movieNameParameterGenerator() {
+    console.log('movieName value:', movieName.value);
+    if (movieName.value === '') {
+        throw new Error('O nome do filme deve ser informado');
+    }
+    return movieName.value.split(' ').join('+');
+}
+
+function movieYearParameterGenerator() {
+    if (movieYear.value === '') {
+        return '';
+    }
+    if (movieYear.value.length !== 4 || Number.isNaN(Number(movieYear.value))) {
+        throw new Error('Ano do filme inválido');
+    }
+    return `&y=${movieYear.value}`;
+}
+
+
+function addToList(movieObject){
+    movieList.push(movieObject); //pegue um elemento e empurre para a lista
+}
+
+function updateUI(movieObject) {
+    //console.log('current movie: ', currentMovie)
+    movieListContainer.innerHTML += `
+    <article id="movie-card-${movieObject.imdbID}"> 
+        <img src="${movieObject.Poster}" alt="Poster de ${movieObject.Title}."> 
+        <button class="remove-button" onclick="{removeFilmFromList('${movieObject.imdbID}')}">
+         <i class="bi bi-trash"></i>
+         remover
+        </button> 
+    </article> `
+}
+
+function removeFilmFromList(id){
+    
+    notie.confirm({
+        text: 'Deseja remover o filme?',
+        submitText: "Sim",
+        cancelText: "Não",
+        position: 'top',
+        submitCallback: function removeMovie() {
+            movieList = movieList.filter(movie => movie.imdbID !== id);
+            document.getElementById(`movie-card-${id}`).remove();
+            updateLocalStorage()
+        } 
+    })
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('movieList', JSON.stringify(movieList));
+}
+
+for (const movieInfo of movieList){
+    updateUI(movieInfo)
+}
+
+$searchButton.addEventListener('click', searchButtonClickHandler);
