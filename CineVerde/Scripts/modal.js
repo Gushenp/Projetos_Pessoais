626f3440607f4656dad32b7d76e03cc32e607244
Gustavo Henrique <143838010+Gushenp@users.noreply.github.com>
2024-09-27 03:16:12 +0000

    adição de animações

--- CineVerde/Scripts/modal.js
+++ CineVerde/Scripts/modal.js
@@ -30,21 +30,21 @@ function creatmodal(data) {
     `
         <h2 id="movie-title">${data.Title} - ${data.Year}</h2>
         <section id="modal-body">
-            <img id="movie-poster" src="${data.Poster}" 
-            alt="poster do filme.">
+            <div id="movie-poster-container">        
+                <img id="movie-poster" src="${data.Poster}" 
+                alt="poster do filme.">
+            </div>
             <div id="movie-info">
+                <div id="movie-cast">
+                    <h4>Sinopse</h4>
+                    <p>${data.Plot} </p>
+                </div>
                 <div id="movie-plot">
                     <h4>Participantes</h4>
                     <p>${data.Actors}</p>
                 </div>
-                <div id="movie-cast">
-                    <h4>${data.Plot} </h4>
-                    <p></p>
-                </div>
                 <div id="movie-genre">
-                    <h4>
-                        Genero
-                    </h4>
+                    <h4>Gênero</h4>
                     <p>${data.Genre}</p>
                 </div>
             </div>
