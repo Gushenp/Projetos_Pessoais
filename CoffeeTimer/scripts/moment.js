cb29b67a502f27f1056dc6b67f8b5a63fd770442
Gustavo Henrique <143838010+Gushenp@users.noreply.github.com>
2024-10-06 19:54:30 +0000

    modified:   CoffeeTimer/index.html deleted:    CoffeeTimer/scripts/function.js modified:   CoffeeTimer/scripts/moment.js modified:   CoffeeTimer/styles/style.css

--- CoffeeTimer/scripts/moment.js
+++ CoffeeTimer/scripts/moment.js
@@ -57,3 +57,108 @@ async function timerExecutation() {
 updateDisplay();
 
 startButton.addEventListener('click', timerExecutation);
+
+//change timer
+
+const functions = document.querySelectorAll('.function');
+const functionsContainer = document.querySelector('.functions-container');
+const timerContainer = document.querySelector('.timer-container');
+
+function addAnimationFC(){
+    functionsContainer.classList.add('AnimationFC')
+    timerContainer.classList.add('AnimationTC')
+}
+
+function removeAnimationFC(){
+    functionsContainer.classList.remove('AnimationFC')
+    timerContainer.classList.remove('AnimationTC')
+}
+
+functionsContainer.addEventListener('mouseover', addAnimationFC)
+functionsContainer.addEventListener('mouseout', removeAnimationFC)
+
+const function25 = document.getElementById('function-25')
+const function50 = document.getElementById('function-50')
+const sbreak = document.getElementById('function-break-5')
+const lbreak = document.getElementById('function-break-30')
+
+function changeTimerTo25() {
+    if (timerActive) {
+        clearInterval(timer);
+    }
+    starTime.minutes = 25;
+    starTime.seconds = 0; 
+    minute = starTime.minutes;
+    second = starTime.seconds
+    timerActive = false;
+    startButton.classList.remove('stop')
+    startButton.classList.add('start')
+    startButton.innerText = 'Start';
+    updateDisplay(); 
+}
+
+function changeTimerTo50() {
+    if (timerActive) {
+        clearInterval(timer);
+    }
+    starTime.minutes = 50;
+    starTime.seconds = 0; 
+    minute = starTime.minutes;
+    second = starTime.seconds
+    timerActive = false;
+    startButton.classList.remove('stop')
+    startButton.classList.add('start')
+    startButton.innerText = 'Start';
+    updateDisplay(); 
+}
+
+function changeTimerToSbreak() {
+    if (timerActive) {
+        clearInterval(timer);
+    }
+    starTime.minutes = 5;
+    starTime.seconds = 0; 
+    minute = starTime.minutes;
+    second = starTime.seconds
+    timerActive = false;
+    startButton.classList.remove('stop')
+    startButton.classList.add('start')
+    startButton.innerText = 'Start';
+    updateDisplay();  
+}
+
+
+function changeTimerToLbreak() {
+    if (timerActive) {
+        clearInterval(timer);
+    }
+    starTime.minutes = 30;
+    starTime.seconds = 0; 
+    minute = starTime.minutes;
+    second = starTime.seconds
+    timerActive = false;
+    startButton.classList.remove('stop')
+    startButton.classList.add('start')
+    startButton.innerText = 'Start';
+    updateDisplay();  
+}
+
+function25.addEventListener('click', changeTimerTo25);
+function50.addEventListener('click', changeTimerTo50);
+sbreak.addEventListener('click', changeTimerToSbreak);
+lbreak.addEventListener('click', changeTimerToLbreak);
+
+function changeColor(element) {
+    element.classList.add('function-animation');
+}
+
+function removeColor(element) {
+    element.classList.remove('function-animation');
+}
+
+functions.forEach(func => {
+    func.addEventListener('mouseover', () => changeColor(func));
+    func.addEventListener('mouseout', () => removeColor(func));
+    func.addEventListener('click', () => select(func));
+});
+
