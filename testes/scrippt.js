aa6fa0b6bdf54887512337fa81c82dc08ee83f78
Gustavo Henrique <143838010+Gushenp@users.noreply.github.com>
2024-11-07 23:50:27 +0000

    - Adição de tasks adicionadas

--- testes/scrippt.js
+++ testes/scrippt.js
@@ -1,5 +1,30 @@
-const square = document.getElementById('object');
+const botao = document.getElementById('botao');
+const body = document.getElementById('bodyy');
 
-square.addEventListener('click', () => {
-    square.classList.add('hidden'); // Adiciona a classe 'hidden' para ocultar o elemento
+let list = [];
+let value = 0;
+
+botao.addEventListener('click', () => {
+    let task = `
+    <div class="object" id="object-${value}">
+        Esse objeto tem o valor: ${value}
+        <button class="excluir" data-id="${value}">Excluir</button>
+    </div>`;
+    
+    body.insertAdjacentHTML('beforeend', task);
+    list.push(value);
+    value++;
+
+    const deleteButtons = document.querySelectorAll('.excluir');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const id = parseInt(event.target.getAttribute('data-id'));
+            list = list.filter(item => item !== id);
+
+            const taskToRemove = document.getElementById(`object-${id}`);
+            if (taskToRemove) {
+                taskToRemove.remove();
+            }
+        });
+    });
 });
